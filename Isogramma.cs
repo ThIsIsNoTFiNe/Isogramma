bc964a46f06a10e1f6e40484b919d4f0912b5e67
Nicholas Marconi <127590111+ThIsIsNoTFiNe@users.noreply.github.com>
2023-05-08 15:52:22 +0200

    .

---
+++ Isogramma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+
+        int lung= word.Length;
+        for (int i = 0; i < lung; i++)
+        {
+            word = word.ToLower();
+        }
+
+        for (int i = 0; i < lung; i++) {
+            if (word[i] == ' ' || word[i] == '-') {
+                continue;  // Salta alla iterazione dopo senza finire la corrente.
+            }
+            for (int j = i + 1; j < lung; j++) {
+                if (word[i] == word[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    }
+
